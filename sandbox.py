b68594e05f912113a0b3496800c268044f2cc3df
John Park <johnpark.pj@gmail.com>
2017-02-01 07:18:44 +0900

    feat: second submission

---
+++ sandbox.py
@@ -0,0 +1,9 @@
+parameters = {
+    'max_depth': [3, 5, None],
+    'min_samples_split': [2, 3, 4, 5],
+    'min_samples_leaf': [1, 2, 3, 4, 5],
+    'min_weight_fraction_leaf': [0, .1, .5],
+    'max_features': [None, 'sqrt', 'log2'],
+    'max_depth': [5, 8, 15, None],
+    'min_samples_split': [2, 5, 10]
+}
