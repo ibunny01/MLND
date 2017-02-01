a9fc7d84737da2137b2754d4d3eeee030e768ec3
John Park <johnpark.pj@gmail.com>
2017-02-01 18:38:33 +0900

    feat: MinMaxScaler added

---
+++ Unsupervised-Learning/Feature-Engineering/Feature-Scaling/MinMaxScaler.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+weights = np.array([[140],[175],[115]]).astype(float)
+scaler = MinMaxScaler()
+scaled_weight = scaler.fit_transform(weights)
+print weights
+print scaled_weight
