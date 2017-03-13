bc046f71ccc7d93386a476a3df4c2ed9cdd0cb07
John Park <johnpark.pj@gmail.com>
2017-03-13 18:14:57 +0900

    feat: Changes in MNIST Trainer
    - Reach 0.99% accuracy
    - Apply twice dropout to fully connected layer
    - Adjust learning rate (*important)
    - Separate loss function up to digit length and each digits

--- MLND-P6-Digit-Recognition/cnn_trainer.py
+++ MLND-P6-Digit-Recognition/cnn_trainer.py
@@ -2,9 +2,9 @@ from __future__ import print_function
 
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
 from PIL import Image
 
+import tensorflow as tf
 from data_loader import MNISTLoader, SVHNLoader
 
 _DEBUG = True
@@ -14,10 +14,48 @@ def _log(message, end='\r\n'):
         print(message, end=end)
 
 class Trainer:
+
+    def __init__(self):
+
+        return
+
+    def weight_variable(self, shape):
+        return tf.Variable(tf.truncated_normal(shape, stddev=0.1), dtype=tf.float32)
+
+    def bias_variable(self, shape):
+        return tf.Variable(tf.zeros(shape), dtype=tf.float32)
+
+    def conv2d(self, x, W):
+        return tf.nn.conv2d(x, W,
+                            strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME')
+
+    def get_conv2d(name, data, patch, d_in, d_out, stride, pooling=None):
+        weights = tf.Variable(tf.truncated_normal([patch, patch, d_in, d_out], stddev=get_conv2d_weights_init_stddev(img_w,img_h,d_in)), name=str('%s_w' % name))
+        biases = tf.Variable(tf.zeros([d_out]), name=str('%s_b' % name))
+        layer = tf.nn.relu(tf.nn.conv2d(data, weights, stride, padding='SAME') + biases)
+        if pooling is not None:
+            layer = tf.nn.max_pool(layer, pooling, pooling, padding='SAME')
+        return weights, biases, layer
+
+    def get_fc(name, data, depth, relu=True):
+        inbound = int(data.get_shape()[1])
+        weights = tf.Variable(tf.truncated_normal([inbound, depth], stddev=math.sqrt(2.0 / inbound), name=str('%s_w' % name)))
+        biases = tf.Variable(tf.zeros([depth]), name=str('%s_b' % name))
+        layer = tf.matmul(data, weights) + biases
+        if relu is True:
+            layer = tf.nn.relu(layer)
+        return weights, biases, layer
+
+class CNNTrainer(Trainer):
     tf_x = None
     tf_y_ = None
 
     tf_keep_prob = None
+    tf_l2_beta = None
 
     train_dataset = None
     test_dataset = None
@@ -51,23 +89,12 @@ class Trainer:
         self.tf_x = tf.placeholder(tf.float32, self.raw_image_shape)
         self.tf_y_ = tf.placeholder(tf.float32, self.label_shape)
 
+        self.tf_learning_rate = tf.placeholder(tf.float32)
+        self.tf_l2_beta = tf.placeholder(tf.float32)
+
         self.tf_keep_prob = tf.placeholder(tf.float32)
 
         return
-
-    def weight_variable(self, shape):
-        return tf.Variable(tf.truncated_normal(shape, stddev=0.1), dtype=tf.float32)
-
-    def bias_variable(self, shape):
-        return tf.Variable(tf.zeros(shape), dtype=tf.float32)
-
-    def conv2d(self, x, W):
-        return tf.nn.conv2d(x, W,
-                            strides=[1, 1, 1, 1], padding='SAME')
-
-    def max_pool_2x2(self, x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1], padding='SAME')
 
     def set_model(self):
         self.is_model_initialized = True
@@ -87,32 +114,32 @@ class Trainer:
         w_conv4 = self.weight_variable([5, 5, 64, 128])
         b_conv4 = self.bias_variable([128])
 
-        w_fc1 = self.weight_variable([4 * 4 * 128, 1024])
-        b_fc1 = self.bias_variable([1024])
+        w_fc1 = self.weight_variable([4 * 4 * 128, 256])
+        b_fc1 = self.bias_variable([256])
 
-        w_fc2 = self.weight_variable([1024, 70])
-        b_fc2 = self.bias_variable([70])
+        w_fc2 = self.weight_variable([256, 128])
+        b_fc2 = self.bias_variable([128])
 
         # weight & bias matrix depends on features
-        w_fc2_len = self.weight_variable([1024, 10])
+        w_fc2_len = self.weight_variable([128, 10])
         b_fc2_len = self.bias_variable([10])
 
-        w_fc2_d1 = self.weight_variable([1024, 10])
+        w_fc2_d1 = self.weight_variable([128, 10])
         b_fc2_d1 = self.bias_variable([10])
 
-        w_fc2_d2 = self.weight_variable([1024, 10])
+        w_fc2_d2 = self.weight_variable([128, 10])
         b_fc2_d2 = self.bias_variable([10])
 
-        w_fc2_d3 = self.weight_variable([1024, 10])
+        w_fc2_d3 = self.weight_variable([128, 10])
         b_fc2_d3 = self.bias_variable([10])
 
-        w_fc2_d4 = self.weight_variable([1024, 10])
+        w_fc2_d4 = self.weight_variable([128, 10])
         b_fc2_d4 = self.bias_variable([10])
 
-        w_fc2_d5 = self.weight_variable([1024, 10])
+        w_fc2_d5 = self.weight_variable([128, 10])
         b_fc2_d5 = self.bias_variable([10])
 
-        w_fc2_d6 = self.weight_variable([1024, 10])
+        w_fc2_d6 = self.weight_variable([128, 10])
         b_fc2_d6 = self.bias_variable([10])
 
         # convolution layer
@@ -132,31 +159,34 @@ class Trainer:
         shape  = h_pool4.get_shape().as_list()
 
         h_pool4_flat = tf.reshape(h_pool4, [-1, shape[1] * shape[2] * shape[3]])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat, w_fc1) + b_fc1)
+
+        h_pool4_flat_dropout = tf.nn.dropout(h_pool4_flat, self.tf_keep_prob, seed=self.seed)
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat_dropout, w_fc1) + b_fc1)
 
         # dropout
         h_fc1_dropout = tf.nn.dropout(h_fc1, self.tf_keep_prob, seed=self.seed)
+        h_fc2 = tf.nn.relu(tf.matmul(h_fc1_dropout, w_fc2)+b_fc2)
 
         # readout layer
-        pred_r_len = tf.matmul(h_fc1_dropout, w_fc2_len) + b_fc2_len
+        pred_r_len = tf.matmul(h_fc2, w_fc2_len) + b_fc2_len
         pred_len = tf.reshape(pred_r_len, [-1,10])
 
-        pred_r_d1 = tf.matmul(h_fc1_dropout, w_fc2_d1) + b_fc2_d1
+        pred_r_d1 = tf.matmul(h_fc2, w_fc2_d1) + b_fc2_d1
         pred_d1 = tf.reshape(pred_r_d1, [-1,10])
 
-        pred_r_d2 = tf.matmul(h_fc1_dropout, w_fc2_d2) + b_fc2_d2
+        pred_r_d2 = tf.matmul(h_fc2, w_fc2_d2) + b_fc2_d2
         pred_d2 = tf.reshape(pred_r_d2, [-1,10])
 
-        pred_r_d3 = tf.matmul(h_fc1_dropout, w_fc2_d3) + b_fc2_d3
+        pred_r_d3 = tf.matmul(h_fc2, w_fc2_d3) + b_fc2_d3
         pred_d3 = tf.reshape(pred_r_d3, [-1,10])
 
-        pred_r_d4 = tf.matmul(h_fc1_dropout, w_fc2_d4) + b_fc2_d4
+        pred_r_d4 = tf.matmul(h_fc2, w_fc2_d4) + b_fc2_d4
         pred_d4 = tf.reshape(pred_r_d4, [-1,10])
 
-        pred_r_d5 = tf.matmul(h_fc1_dropout, w_fc2_d5) + b_fc2_d5
+        pred_r_d5 = tf.matmul(h_fc2, w_fc2_d5) + b_fc2_d5
         pred_d5 = tf.reshape(pred_r_d5, [-1,10])
 
-        pred_r_d6 = tf.matmul(h_fc1_dropout, w_fc2_d6) + b_fc2_d6
+        pred_r_d6 = tf.matmul(h_fc2, w_fc2_d6) + b_fc2_d6
         pred_d6 = tf.reshape(pred_r_d6, [-1,10])
 
         # loss calculation
@@ -171,27 +201,31 @@ class Trainer:
         softmax_d4 = tf.nn.softmax_cross_entropy_with_logits(labels=tf.reshape(self.tf_y_, (-1,7,10))[:,4,:],
                                                               logits=pred_d4)
         softmax_d5 = tf.nn.softmax_cross_entropy_with_logits(labels=tf.reshape(self.tf_y_, (-1,7,10))[:,5,:],
-                                                              logits=pred_d5)
+                                                              logits=pred_d5) 
         softmax_d6 = tf.nn.softmax_cross_entropy_with_logits(labels=tf.reshape(self.tf_y_, (-1,7,10))[:,6,:],
                                                               logits=pred_d6)
 
-        self.tf_loss = tf.reduce_mean(softmax_len)
-        self.tf_loss += tf.reduce_mean(softmax_d1)
-        self.tf_loss += tf.reduce_mean(softmax_d2)
-        self.tf_loss += tf.reduce_mean(softmax_d3)
-        self.tf_loss += tf.reduce_mean(softmax_d4)
-        self.tf_loss += tf.reduce_mean(softmax_d5)
-        self.tf_loss += tf.reduce_mean(softmax_d6)
+        regularization = tf.nn.l2_loss(w_fc1) + tf.nn.l2_loss(b_fc1)
+        self.tf_loss = self.tf_l2_beta * regularization
+
+        self.tf_loss = tf.maximum(tf.reduce_mean(softmax_len), tf.constant(0.0))
+        self.tf_loss += tf.maximum(tf.reduce_mean(softmax_d1), tf.constant(0.0))
+        self.tf_loss += tf.maximum(tf.reduce_mean(softmax_d2), tf.constant(0.0))
+        self.tf_loss += tf.maximum(tf.reduce_mean(softmax_d3), tf.constant(0.0))
+        self.tf_loss += tf.maximum(tf.reduce_mean(softmax_d4), tf.constant(0.0))
+        self.tf_loss += tf.maximum(tf.reduce_mean(softmax_d5), tf.constant(0.0))
+        # self.tf_loss += tf.reduce_mean(softmax_d6)
 
         # loss optimizer 
-        self.tf_optimizer = tf.train.AdamOptimizer(1e-4).minimize(self.tf_loss)
+        self.tf_optimizer = tf.train.AdamOptimizer(learning_rate=self.tf_learning_rate).minimize(self.tf_loss)
 
         # accuracy calculation
-        predict = tf.reshape(tf.stack([pred_len, pred_d1, pred_d2, pred_d3, pred_d4, pred_d5, pred_d6], axis=1), (-1, 70))
-        print(predict)
+        pred_combined = tf.stack([pred_len, pred_d1, pred_d2, pred_d3, pred_d4, pred_d5, pred_d6], axis=1)
+        predict = tf.reshape(pred_combined, (-1, 70))
 
-        import pdb; pdb.set_trace()
-        
+        print(predict)
+        print(self.tf_y_)
+
         is_correct_pred = tf.equal(
             tf.argmax(predict, 1), tf.argmax(self.tf_y_, 1))
         self.tf_accuracy = tf.reduce_mean(tf.cast(is_correct_pred, tf.float32))
@@ -246,61 +280,46 @@ class Trainer:
                     self.train_dataset[4][shuffle_step*batch_size:shuffle_step*batch_size+batch_size],
                     self.train_dataset[1][shuffle_step*batch_size:shuffle_step*batch_size+batch_size]]
 
+                feed_train ={self.tf_x: batch[0],
+                             self.tf_y_: batch[1],
+                             self.tf_learning_rate: 3.1e-4,
+                             self.tf_l2_beta: 16e-4,
+                             self.tf_keep_prob: 0.5}
+
+                feed_test = {self.tf_x: self.test_dataset[0],
+                             self.tf_y_: self.test_dataset[4],
+                             self.tf_learning_rate: 3.1e-4,
+                             self.tf_l2_beta: 16e-4,
+                             self.tf_keep_prob: 1.}
+
                 # Logging training
                 if not i % 100:
 
                     # Check batch images for the training
                     if False:
+
                         fig = plt.figure(figsize=(12, 1), dpi=80)
+
                         for i in range(12):
                             plt.subplot(1, 12, i+1)
                             plt.title(batch[2][i])
-                            plt.imshow(batch[0][i,:,:].reshape(64,64),
+                            plt.imshow(batch[0][i,:,:].reshape(64, 64),
                                        interpolation='nearest',
                                        cmap='Greys')
                             plt.tight_layout()
+
                         plt.show()
 
                     _loss, _train_accuracy = sess.run([self.tf_loss, self.tf_accuracy],
-                                                      feed_dict={self.tf_x: batch[0],
-                                                                 self.tf_y_: batch[1],
-                                                                 self.tf_keep_prob: 1.0})
+                                                      feed_dict=feed_test)
 
                     print("step %d, loss : %g / training_accuracy: %g" %
                           (i, _loss, _train_accuracy))
 
                 # Do training
+                self.tf_optimizer.run(feed_dict=feed_train)
 
-                # print(self.tf_debug1.eval(
-                #     feed_dict={self.x: batch[0],
-                #                self.y_: batch[1],
-                #                self.keep_prob: 0.5}))
-
-                # print(self.tf_debug2.eval(
-                #     feed_dict={self.x: batch[0],
-                #                self.y_: batch[1],
-                #                self.keep_prob: 0.5}))
-
-                # print(self.tf_debug3.eval(
-                #     feed_dict={self.x: batch[0],
-                #                self.y_: batch[1],
-                #                self.keep_prob: 0.5}))
-
-                # print(self.debug4.eval(
-                #     feed_dict={self.x: batch[0],
-                #                self.y_: batch[1],
-                #                self.keep_prob: 0.5}))
-
-                self.tf_optimizer.run(
-                    feed_dict={self.tf_x: batch[0],
-                               self.tf_y_: batch[1],
-                               self.tf_keep_prob: 0.5})
-
-            print('Test Accuracy %g' % (self.tf_accuracy.eval(
-                feed_dict={self.tf_x: batch[0],
-                           self.tf_y_: batch[1],
-                           self.tf_keep_prob: 1.0})))
-
+            print('Test Accuracy %g' % (self.tf_accuracy.eval(feed_dict=feed_test)))
         return
 
 
@@ -310,7 +329,7 @@ def train_for_mnist_normal():
     loader = MNISTLoader()
     loader.init_data()
 
-    trainer = Trainer([None, 28, 28, 1], [None, 10])
+    trainer = CNNTrainer([None, 28, 28, 1], [None, 10])
 
     _log('data loading...', end='\r\n')
 
@@ -349,7 +368,7 @@ def train_for_mnist_synthetic():
     loader = MNISTLoader()
     loader.init_data()
 
-    trainer = Trainer([None, 64, 64, 1], [None, 70])
+    trainer = CNNTrainer([None, 64, 64, 1], [None, 70])
 
     _log('data loading...', end='\r\n')
 
